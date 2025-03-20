181348c70be372cab50b23f710c4ce656ecaa4d1
Nalin <nalindalal2004@gmail.com>
2025-03-20 23:25:37 +0530

    improved syntax for hashmap assignment

--- src/hash_map2.rs
+++ src/hash_map2.rs
@@ -1,15 +1,22 @@
 //Write a function that takes a vector of tuples (each tuple containing a key and a value) and returns a Hashmap where the keys are the unique keys from the input tuples and the values are vectors of all corresponding values associated with each key.
 use std::collections::HashMap;
-fn group_values_by_key(pairs: Vec<(String, i32)>) -> HashMap<String,i32>{
+fn group_values_by_key(pairs: Vec<(String, i32)>) -> HashMap<String, Vec<i32>> {
     //sol->
     let mut hm = HashMap::new();
-    for(key,value) in vec { hm.insert(k:key, v:value);}return hm;
+    for (key, value) in pairs {
+        hm.entry(key).or_insert(Vec::new()).push(value);
+    }
+    hm
 }
-fn main(){
-    let pairs:Vec<(String,i32)>=vec![
-        (String::from("John"),21),
-        (String::from("raman"),31)
+fn main() {
+    let pairs: Vec<(String, i32)> = vec![
+        (String::from("John"), 21),
+        (String::from("raman"), 31),
+        (String::from("Naman"), 25),
+        (String::from("Nitesh"), 35),
     ];
-let grouped_pairs = group_values_by_key(pairs);
-for (key, value) in grouped_patrs { println! ("{}: {:?}", key, value);}
+    let grouped_pairs = group_values_by_key(pairs);
+    for (key, value) in grouped_pairs {
+        println!("{}: {:?}", key, value);
+    }
 }
